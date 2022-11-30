d90429871d0bda14aaabfe01e783feda6d7ed375
coque <coquelet.samuel@gmail.com>
2022-11-30 11:21:35 +0100

    time tracking front ...

---
+++ assets/app.ts
@@ -0,0 +1,9 @@
+import './styles/styles.scss';
+
+let monString: string = 'string';
+let monNombre: number = 54674543;
+
+console.log(monNombre);
+console.log(monString);
+
+let result: number = monNombre + parseInt(monString);
