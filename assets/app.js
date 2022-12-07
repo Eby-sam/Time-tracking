584bcf9ae93f2d3e9c4a8320b1db780dd1c2a6c0
coque <coquelet.samuel@gmail.com>
2022-12-07 13:32:29 +0100

    creat project default 1/3

--- assets/app.js
+++ assets/app.js
@@ -1,38 +1,8 @@
-"use strict";
-exports.__esModule = true;
-require("./sass/style.scss");
 var but = document.getElementById('but');
 var hour = document.getElementById('hours');
 var days = document.getElementById('days');
-but.addEventListener("click", function () {
-    var container = '<div class="container">\n' +
-        '                <div class="divTitle">\n' +
-        '                    <h2>Default Project</h2>\n' +
-        '                </div>\n' +
-        '                <div class="containerBlock">\n' +
-        '                    <div class="time">\n' +
-        '                        <div class="oclock">\n' +
-        '                            <i class="fa-regular fa-clock"></i>\n' +
-        '                            <p><span class="hours">0</span> h</p>\n' +
-        '                        </div>\n' +
-        '                        <div class="dates">\n' +
-        '                            <i class="fa-solid fa-calendar-days"></i>\n' +
-        '                            <p>Il y a <span class="days">0</span> jours</p>\n' +
-        '                        </div>\n' +
-        '                    </div>\n' +
-        '                    <div class="taches">\n' +
-        '\n' +
-        '                    </div>\n' +
-        '                </div>\n' +
-        '                <div class="barre">\n' +
-        '                    <div class="corbeil">\n' +
-        '                        <i class="fa-solid fa-trash"></i>\n' +
-        '                    </div>\n' +
-        '                    <div class="look">\n' +
-        '                        <i class="fa-solid fa-eye"></i>\n' +
-        '                    </div>\n' +
-        '                    <div class="new">+ Ajouter une tâche</div>\n' +
-        '                </div>\n' +
-        '            </div>';
-    document.body.append(container);
-});
+var containerTop = document.getElementById('containerTop');
+function creatProject() {
+    containerTop.innerHTML = "<div class=\"container\">\n                        <div class=\"divTitle\">\n                            <h2>Default Project</h2>\n                        </div>\n                        <div class=\"containerBlock\">\n                            <div class=\"time\">\n                                <div class=\"oclock\">\n                                    <i class=\"fa-regular fa-clock\"></i>\n                                    <p><span class=\"hours\">0</span> h</p>\n                                </div>\n                                <div class=\"dates\">\n                                    <i class=\"fa-solid fa-calendar-days\"></i>\n                                    <p>Il y a <span class=\"days\">0</span> jours</p>\n                                </div>\n                            </div>\n                            <div class=\"taches\">\n        \n                            </div>\n                        </div>\n                        <div class=\"barre\">\n                            <div class=\"corbeil\">\n                                <i class=\"fa-solid fa-trash\"></i>\n                            </div>\n                            <div class=\"look\">\n                                <i class=\"fa-solid fa-eye\"></i>\n                            </div>\n                            <div class=\"new\">+ Ajouter une t\u00E2che</div>\n                        </div>\n                    </div>";
+}
+but.addEventListener('click', creatProject);
--- assets/app.ts
+++ assets/app.ts
@@ -1,40 +1,38 @@
-import './sass/style.scss';
-
-
 let but = document.getElementById('but') as HTMLElement;
 let hour = document.getElementById('hours') as HTMLElement;
 let days = document.getElementById('days') as HTMLElement;
+let containerTop = document.getElementById('containerTop') as HTMLElement;
 
+function creatProject() {
+    containerTop.innerHTML = `<div class="container">
+                        <div class="divTitle">
+                            <h2>Default Project</h2>
+                        </div>
+                        <div class="containerBlock">
+                            <div class="time">
+                                <div class="oclock">
+                                    <i class="fa-regular fa-clock"></i>
+                                    <p><span class="hours">0</span> h</p>
+                                </div>
+                                <div class="dates">
+                                    <i class="fa-solid fa-calendar-days"></i>
+                                    <p>Il y a <span class="days">0</span> jours</p>
+                                </div>
+                            </div>
+                            <div class="taches">
+        
+                            </div>
+                        </div>
+                        <div class="barre">
+                            <div class="corbeil">
+                                <i class="fa-solid fa-trash"></i>
+                            </div>
+                            <div class="look">
+                                <i class="fa-solid fa-eye"></i>
+                            </div>
+                            <div class="new">+ Ajouter une tâche</div>
+                        </div>
+                    </div>`;
+}
 
-but.addEventListener("click", function() {
-    let container = '<div class="container">\n' +
-        '                <div class="divTitle">\n' +
-        '                    <h2>Default Project</h2>\n' +
-        '                </div>\n' +
-        '                <div class="containerBlock">\n' +
-        '                    <div class="time">\n' +
-        '                        <div class="oclock">\n' +
-        '                            <i class="fa-regular fa-clock"></i>\n' +
-        '                            <p><span class="hours">0</span> h</p>\n' +
-        '                        </div>\n' +
-        '                        <div class="dates">\n' +
-        '                            <i class="fa-solid fa-calendar-days"></i>\n' +
-        '                            <p>Il y a <span class="days">0</span> jours</p>\n' +
-        '                        </div>\n' +
-        '                    </div>\n' +
-        '                    <div class="taches">\n' +
-        '\n' +
-        '                    </div>\n' +
-        '                </div>\n' +
-        '                <div class="barre">\n' +
-        '                    <div class="corbeil">\n' +
-        '                        <i class="fa-solid fa-trash"></i>\n' +
-        '                    </div>\n' +
-        '                    <div class="look">\n' +
-        '                        <i class="fa-solid fa-eye"></i>\n' +
-        '                    </div>\n' +
-        '                    <div class="new">+ Ajouter une tâche</div>\n' +
-        '                </div>\n' +
-        '            </div>';
-    document.body.append(container);
-})
+but.addEventListener('click', creatProject);
